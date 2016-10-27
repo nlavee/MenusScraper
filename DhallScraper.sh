21f8a5fdee1f8be352821863c7f7bee169290008
Anh Vu Nguyen <anhvu.nguyenlam@gmail.com>
2016-10-27 10:33:27 -0400

    First commit. Got pdf, got text, parsing right now.

---
+++ DhallScraper.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Get the links to dhall menus
+curl "https://www.skidmore.edu/diningservice/menus/" > menus.html
+grep -oh "\\/diningservice\/menus\/[Dinner|Lunch].*\.pdf\b" menus.html > links.txt
+
+## Go through each line and download the menu pdfs
+input="links.txt"
+while IFS= read -r var
+do
+    curl -O "https://www.skidmore.edu$var"
+done < "$input"
+
+## Remove unneeded textfiles
+rm menus.html
+rm links.txt
+
+# Install needed tools
+## sudo apt-get install poppler-utils
+
+for f in *.pdf; do
+  pdftotext -layout "$f"
+done
+
+# Remove unneeded pdf files
+rm *.pdf
+
+./ScrapeDhall.pl
---
+++ ScrapeDhall.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+
+use strict;
+
+print "\n===============================================================================\n";
+print "\nHello there, seems like you want to know what's available in Dhall Today.\n";
+print "\n===============================================================================\n";
+
+my $debug = 1;
+
+my @array = `ls *.txt`;
+my %hash;
+foreach my $element(@array) {
+    print $element if $debug;
+    chomp($element);
+
+    open($STDIN, "<", $element) or die("Error opening file $element. Please double-check.\n");
+    local $/;
+    while(my $line = <$STDIN>) {
+        # Go through line by line
+        # print $line if $debug; 
+        &matches($line);
+    }
+
+    # print "\n===============================================================================\n";
+    # print "\nEND ONE FILE\n";
+    # print "\n===============================================================================\n";
+
+    close $STDIN;
+}
+
+sub matches {
+    my ($file) = @_;
+    my $Emily;
+    my $Diner;
+    my $Pasta;
+    my $Global;
+
+    # print $file if $debug;
+    if($file =~ /(Emily's\ Entr(.*))(THE\ DINER)/s) {
+        # print $1 . "\n" if $debug;
+        $Emily = $1;
+    }
+    if($file =~ /(THE\ DINER(.*))(PASTA)/s) {
+        # print $1 . "\n" if $debug;
+        $Diner = $1;
+    }
+    if($file =~ /(PASTA(.*))(GLOBAL)/s) {
+        # print $1 . "\n" if $debug;
+        $Pasta = $1;
+    }
+    if($file =~ /(GLOBAL(.*))/s) {
+        # print $1 . "\n" if $debug;
+        $Global = $1;
+    }
+
+    &matchesEmily($Emily);
+
+}
+
+sub matchesEmily {
+    my ($Emily) = @_;
+    while(1) {
+        if($Emily =~ /()/) {
+            print $1 . "\n";
+            $Emily = $';
+        }  
+    }
+}
