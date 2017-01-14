05015011abeeae1415ee018bcce10b58f609f7f8
nlavee <anhvu.nguyenlam@gmail.com>
2017-01-14 22:41:15 +0300

    Added some commenting to make the first part of choosing the targeting week clearer.

--- ScrapeDhall.pl
+++ ScrapeDhall.pl
@@ -11,22 +11,25 @@ print "\n=======================================================================
 my $cycle;
 
 {
-    my $cal = `ls resources/academiccalendar2016.txt`;
+    my $cal = `ls resources/academiccalendar20*.txt`; # Can be any year in the format of 20**
     chomp $cal;
 
     my $pattern = "(.*)Classes Begin";
-    my $curr_date = `date +"%U"`;
+    my $curr_date = `date +"%U"`; # this gives the week of the year
 
     open($STDIN, "<", $cal) or die ("Could not find academic calendar\n");
     local $/;
 
     my $line = <$STDIN>;
 
+    # Searching within the first two instances of "Classes Begin" (Fall & Spring)
     for (my $i = 0; $i < 2; $i++) {
         if ($line =~ /^$pattern$/m) {
             my $date_truncate = join ( ' ', split (/\s+/, $1) );
-            my $date_converted = `date -d '$date_truncate' '+%U'`;
+            my $date_converted = `date -d '$date_truncate' '+%U'`; # Getting the week from date truncate
 
+            # Calculating cycle according to how many weeks have passed mod 4
+            # There are only 4 cycles
             if ($curr_date >= $date_converted){
                 $cycle = ($curr_date - $date_converted) % 4;
 
@@ -41,13 +44,18 @@ my $cycle;
     close $STDIN;
 }
 
+# Need to double check cycle
+print "You are currently at cycle: $cycle.\n";
+
 my $curr_day = -7 + (`date +"%w"`+ 1);
 if ($curr_day == 7) {
     $curr_day = -7;
 }
 #TODO: Add a skip for Thanksgiving and Spring Break?
 
+## Parsing the menu according to cycle
 my $debug = 1;
+$cycle = 1 if $debug;
 
 my @array = `find resources/ -regextype posix-extended -regex '(.*)(Cycle)$cycle(.*)'`;
 my %hash;
